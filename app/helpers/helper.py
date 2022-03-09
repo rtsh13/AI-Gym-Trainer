81bd957292d365faae8c0a909750a3cb164a21f5
dinakar <dinakarbl444@gmail.com>
2022-03-09 23:14:03 +0530

    Added a function

--- app/helpers/helper.py
+++ app/helpers/helper.py
@@ -1,4 +1,8 @@
 import numpy as np
+import mediapipe as mp
+drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
 
 def compute(first_angle,second_angle, third_angle):
     first_angle = np.array(first_angle) # First
@@ -12,3 +16,13 @@ def compute(first_angle,second_angle, third_angle):
         angle = 360-angle
         
     return angle 
+def get_coordinates(landmarks):
+    shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+    elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+    wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+    left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+    left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+    right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+    right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+    right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+    return (shoulder,elbow,wrist,left_hip,left_knee,right_shoulder,right_elbow,right_wrist)
