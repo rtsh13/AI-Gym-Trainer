1430a7bd03f9da48243db2e75768750cff02ef8e
Ruchit Singh <ruchitsingh13@gmail.com>
2022-05-16 23:27:49 +0530

    conditional rendering

--- gymUI/src/app/info/info.component.ts
+++ gymUI/src/app/info/info.component.ts
@@ -12,7 +12,7 @@ export class InfoComponent implements OnInit {
 
   form: FormGroup;
   equipmentList:string[]=['Barbell','Dumbell'];
-  exerciseList:string[]=['Squats','Bicep curls','Side leg lift','Diamond pushups','Normal pushups','Jumping jacks'];
+  exerciseList:string[]=['Squats','Bicep curls','Side leg lift','Diamond pushups','Normal pushups','Jumping jacks','Lunges','Concentration Curls'];
   cameraList:string[]=['External','Internal'];
 
   constructor(private fb:FormBuilder,private http:HttpClient,private router:Router,private route:ActivatedRoute) { }
@@ -36,6 +36,30 @@ export class InfoComponent implements OnInit {
     this.http.post('http://127.0.0.1:5000/sendData',data).subscribe(result=>{
       console.log(result);
     });
+    if (data.exercise == 'Bicep curls'){
+      window.location.href = "http://127.0.0.1:5000/biceps"  
+    }
+    if (data.exercise == 'Squats'){
+      window.location.href = "http://127.0.0.1:5000/squats"  
+    }  
+    if (data.exercise == 'Side leg lift'){
+      window.location.href = "http://127.0.0.1:5000/biceps"  
+    }
+    if (data.exercise == 'Diamond pushups'){
+      window.location.href = "http://127.0.0.1:5000/diamondPushups"  
+    }
+    if (data.exercise == 'Normal pushups'){
+      window.location.href = "http://127.0.0.1:5000/diamondPushups"  
+    }
+    if (data.exercise == 'Jumping jacks'){
+      window.location.href = "http://127.0.0.1:5000/jumpingJacks"  
+    }
+    if (data.exercise == 'Lunges'){
+      window.location.href = "http://127.0.0.1:5000/lunges"  
+    }
+    if (data.exercise == 'Concentration Curls'){
+      window.location.href = "http://127.0.0.1:5000/concentrationCurls"  
+    }
   }
 
   onClose(){
